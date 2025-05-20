af11699c51924668ea56410916bdb17f7f63ca07
Nebula3832 <pavlezuzic@gmail.com>
2025-05-20 14:46:40 +0200

    Add files via upload

---
+++ pavle,RuBeSA+LorispYthonPythonIgra (1).py
@@ -0,0 +1,49 @@
+import pygame
+pygame.init()
+
+SCREEN_WIDTH=1600
+SCREEN_HEIGHT=900
+# Set up the game window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Moon escape")
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the screen with a color
+    screen.fill((255, 255, 255))  # This fills the screen with blue
+    pygame.display.update()
+
+pygame.quit()
+
+
+# Player settings
+
+# Create a surface and pass in a tuple containing its length and width
+surf = pygame.Surface((50, 50))
+
+# Give the surface a color to separate it from the background
+surf.fill((0, 0, 0))
+rect = surf.get_rect()
+screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+pygame.display.flip()
+# Put the center of surf at the center of the display
+surf_center = (
+    (SCREEN_WIDTH-surf.get_width())/2,
+    (SCREEN_HEIGHT-surf.get_height())/2
+)
+
+# Draw surf at the new coordinates
+screen.blit(surf, surf_center)
+pygame.display.flip()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((0, 0, 0))
+        self.rect = self.surf.get_rect()
